3648b80d22a2d93ead21f7a1d4044e73978eac1b
Łukasz Duda <lukasz.duda.pl@gmail.com>
2022-01-11 22:06:44 +0100

    Test command

--- warehouse/command_test.py
+++ warehouse/command_test.py
@@ -5,7 +5,7 @@ import json
 class CommandTest(unittest.TestCase):
 
     def test_receive_product(self):
-        request_data = json.dumps({ "sku": "abc", "quantity": 5})
+        request_data = json.dumps({ "sku": "abc", "quantity": 5 })
         headers = {'Content-Type': 'application/json'}
 
         response = requests.post('http://localhost:5000/products/receive', data=request_data, headers=headers)
